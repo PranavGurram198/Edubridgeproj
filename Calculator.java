ad64edd49ecc8ece58551e65d66f909747ebd838
PranavGurram198 <101916625+PranavGurram198@users.noreply.github.com>
2022-04-11 13:20:57 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+
+public class Calculator {
+	public int add(int num1,int num2)
+	{
+		return num1+num2;
+	}
+	public int sub(int num1,int num2)
+	{
+		return num1-num2;
+	}
+	public int mult(int num1,int num2)
+	{
+		return num1*num2;
+	}
+	public int div(int num1,int num2)
+	{
+		return num1/num2;
+	}
+	public int square(int num1)
+	{
+		return num1*num1;
+	}
+    public int remainder(int num1, int num2)
+    {
+    	return num1%num2;
+    }
+}
---
+++ TestCalculator.java
@@ -0,0 +1,16 @@
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator=new Calculator();
+        System.out.println(calculator.add(10, 20));
+        System.out.println(calculator.sub(40, 20));
+        System.out.println(calculator.mult(10, 20));
+        System.out.println(calculator.div(80, 40));
+        System.out.println(calculator.square(16));
+        System.out.println(calculator.remainder(80, 20));
+        
+	}
+
+}
