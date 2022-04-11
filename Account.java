1de88a154e54bc1a2553ee40717a357ae05a5562
PranavGurram198 <101916625+PranavGurram198@users.noreply.github.com>
2022-04-11 13:21:40 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,8 @@
+
+public class Account {
+	void account(String name, float deposit, float withdraw, float checkbalance)
+	{
+		System.out.println(name+" "+deposit+" "+withdraw+" "+checkbalance);		
+	}
+	
+}
